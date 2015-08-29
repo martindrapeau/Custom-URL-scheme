6cd8c592024b5369794baecef11385fc8eb0f4a2
Martin Drapeau <martindrapeau@gmail.com>
2015-08-28 21:47:13 -0400

    Removed callbacks from this file.

--- www/ios/LaunchMyApp.js
+++ www/ios/LaunchMyApp.js
@@ -1,15 +1,3 @@
 "use strict";
 
-window._openURL = null;
-function handleOpenURL(url) {
-  setTimeout(function() {
-    window._openURL = url;
-  }, 0);
-}
 
-window._openUrl = null;
-function handleOpenUrl(url) {
-  setTimeout(function() {
-    window._openUrl = url;
-  }, 0);
-}
