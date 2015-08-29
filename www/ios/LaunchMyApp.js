2c2c0af11e191d4bed00901866c18ab70e853962
Martin Drapeau <martindrapeau@gmail.com>
2015-08-28 21:30:17 -0400

    Update LaunchMyApp.js

--- www/ios/LaunchMyApp.js
+++ www/ios/LaunchMyApp.js
@@ -1,11 +1,13 @@
 "use strict";
 
-
+window._openURL = null;
 function handleOpenURL(url) {
   setTimeout(function() {
     window._openURL = url;
   }, 0);
 }
+
+window._openUrl = null;
 function handleOpenUrl(url) {
   setTimeout(function() {
     window._openUrl = url;
