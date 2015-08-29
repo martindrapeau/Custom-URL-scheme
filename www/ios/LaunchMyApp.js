8e95733ca2cc1fd4e67fe6157e936458119a524b
Martin Drapeau <martindrapeau@gmail.com>
2015-08-28 21:04:31 -0400

    Handlers

--- www/ios/LaunchMyApp.js
+++ www/ios/LaunchMyApp.js
@@ -1,9 +1,13 @@
 "use strict";
 
-/*
- Q: Why an empty file?
- A: iOS doesn't need plumbing to get the plugin to work, so..
-    - Including no file would mean the import in index.html would differ per platform.
-    - Also, using one version and adding a userAgent check for Android feels wrong.
-    - And if you're not using PhoneGap Build, you could paste your handleOpenUrl JS function here.
-*/
+
+function handleOpenURL(url) {
+  setTimeout(function() {
+    window._openURL = url;
+  }, 250);
+}
+function handleOpenUrl(url) {
+  setTimeout(function() {
+    window._openUrl = url;
+  }, 250);
+}
